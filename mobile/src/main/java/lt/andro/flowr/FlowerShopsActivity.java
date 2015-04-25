1a461d939b949e57d7308c087c05dc4090d1b989
Vilius Kraujutis <viliusk@gmail.com>
2015-04-25 11:58:16 +0300

    WIP add geofences

--- mobile/src/main/java/lt/andro/flowr/FlowerShopsActivity.java
+++ mobile/src/main/java/lt/andro/flowr/FlowerShopsActivity.java
@@ -142,4 +142,40 @@ public class FlowerShopsActivity extends FragmentActivity {
             }
         }
     }
+
+//    private GeofencingRequest getGeofencingRequest() {
+//        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+//        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+//        builder.addGeofences(mGeofenceList);
+//        return builder.build();
+//    }
+//
+//    private void enableGeofences() {
+//        mGeofenceList.add(new Geofence.Builder()
+//                // Set the request ID of the geofence. This is a string to identify this
+//                // geofence.
+//                .setRequestId(entry.getKey())
+//
+//                .setCircularRegion(
+//                        entry.getValue().latitude,
+//                        entry.getValue().longitude,
+//                        Constants.GEOFENCE_RADIUS_IN_METERS
+//                )
+//                .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+//                        Geofence.GEOFENCE_TRANSITION_EXIT)
+//                .build());
+//    }
+//
+//    private PendingIntent getGeofencePendingIntent() {
+//        // Reuse the PendingIntent if we already have it.
+//        if (mGeofencePendingIntent != null) {
+//            return mGeofencePendingIntent;
+//        }
+//        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+//        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+//        // calling addGeofences() and removeGeofences().
+//        return PendingIntent.getService(this, 0, intent, PendingIntent.
+//                FLAG_UPDATE_CURRENT);
+//    }
 }
